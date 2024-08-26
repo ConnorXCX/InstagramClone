2df6933e839616411b5354192da206ef5252033f
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-08-26 00:21:02 -0400

    Add Chakra, react-icons, react-router-dom dependencies and setup HomePage component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

bca971cd50bf84e2b5e58abb0a1a7d0d0b503ec3
Connor Thomas <ConnorNoelThomas@gmail.com>
2024-08-26 03:25:30 -0400

    Create PageLayout and Sidebar components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayout from "./layouts/PageLayout/PageLayout";
 
 function App() {
   return (
-    <>
+    <PageLayout>
+      {/* Instead of adding the Sidebar component to every page, we can add it only once to the PageLayout component and wrap the children with it. This way, we can have a sidebar on every page except the AuthPage. */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayout>
   );
 }
 
